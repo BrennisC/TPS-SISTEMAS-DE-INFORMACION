b7a22e88c5f326755d1dea0a17ed8f8d8575b5e3
BrennisC <brennis.castro@unas.edu.pe>
2026-05-24 20:27:14 -0500

    mantener la estructura

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="sistema_admision_unas", plugins=[rx.plugins.TailwindV3Plugin()]
+    app_name="sistema_admisi_n_unas", plugins=[rx.plugins.TailwindV3Plugin()]
 )
